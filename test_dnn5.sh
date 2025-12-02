29652d10f91552a1ebf9fe380898032fd14dcd22
Mituvinci <ha00014@mix.wvu.edu>
2025-12-02 17:25:51 -0500

    Removed irrelevant  files

--- test_dnn5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Test DNN5 training using existing generic SLURM template
-# Uses max_epochs=1500 with early_stopping_patience=100
-
-./scripts/run_train_dl.sh deepdense reh ./models/test_dnn5/ --trials 3 --cv 2
--- test_rf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Test Random Forest training using existing generic SLURM template
-
-./scripts/run_train_tml.sh random_forest reh ./models/test_rf/ --trials 3 --cv 2 --feature-selection SelectKBest
